976a044817d1c168d5ccf1c6d40bca50a37814f3
LuxuriousBots <165535381+LuxuriousBots@users.noreply.github.com>
2024-04-01 01:13:25 +0530

    Update help.py

--- Bikash/utils/inline/help.py
+++ Bikash/utils/inline/help.py
@@ -13,11 +13,11 @@ def help_pannel(_, START: Union[bool, int] = None):
         InlineKeyboardButton(
             # text=_["BACK_BUTTON"],
             text="Updates",
-            url=f"https://t.me/BikashGadgetsTech",
+            url=f"https://t.me/Luxurious_Network",
         ),
         InlineKeyboardButton(
             text="Creator",
-            url=f"t.me/BikashHalder",
+            url=f"t.me/Itz_Luxurious",
         ),
         InlineKeyboardButton(
             text="Close", callback_data=f"close"
